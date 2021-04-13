d4aab17e85eff8e2e8afefc8e673724e4745f9bb
Pashtetollo <oliksander007@ukr.net>
2021-04-14 00:30:15 +0300

    adding 201, 204 and 410 status codes

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
   
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+import re
 
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+deleted = set()
 
 class Fruit(db.Model):
     __tablename__ = 'fruit'
@@ -18,6 +22,7 @@ class Fruit(db.Model):
     color = db.Column(db.String(20), unique=False, nullable=False)
     price = db.Column(db.Integer, unique=False, nullable=False)
     season = db.Column(db.String(20), unique=False, nullable=False)
+
 
 class FruitSchema(ma.Schema):
     class Meta:
@@ -32,59 +37,86 @@ def get_fruits():
 @app.route("/name=<name>", methods=["GET"])
 def get_fruits_by_name(name):
     fruit = Fruit.query.filter(Fruit.name == name)
+    if not fruit:
+        abort(404)
     return jsonify(FruitSchema(many=True).dump(fruit))
-
 
 @app.route("/name=<name>&color=<color>&season=<season>&price&lt=<price>", methods=["GET"])
 def get_ultimate_fruit(name, color, season, price):
     fruit = Fruit.query.filter(
-        #Чому пошук залежить від порядку при операторі or? (якщо поміняти місцями то приймає лише "any" а так працюють обидві умови)
+        #Чому пошук залежить від порядку при операторі or ? (якщо поміняти місцями то приймає лише "any" а так працюють обидві умови)
         (name == "any") or (Fruit.name == name),             
         (color == "any") or (Fruit.color == color),
         (season == "any") or (Fruit.season == season),
         (price == "any") or (Fruit.price <= price))
+    if not fruit:
+        abort(404)
     return jsonify(FruitSchema(many=True).dump(fruit))
     
 @app.route("/color=<color>", methods=["GET"])
 def get_fruits_by_color(color):
     fruit = Fruit.query.filter(Fruit.color == color)
+    if not fruit:
+        abort(404)
     return jsonify(FruitSchema(many=True).dump(fruit))
 
 @app.route("/season=<season>", methods=["GET"])
 def get_fruits_by_season(season):
     fruit = Fruit.query.filter(Fruit.season == season)
+    if not fruit:
+        abort(404)
     return jsonify(FruitSchema(many=True).dump(fruit))
 
 @app.route("/price&lt=<price>", methods=["GET"])
 def get_fruits_by_price(price):
     fruit = Fruit.query.filter(Fruit.price <= price)
+    if not fruit:
+        abort(404)
     return jsonify(FruitSchema(many=True).dump(fruit))
 
 @app.route("/<id>", methods=["GET"])
 def get_fruit(id):
     fruit = Fruit.query.get(id)
     if not fruit:
-        abort(404)
+        if id in deleted:
+            abort(410)
+        else:
+            abort(404)
     return jsonify(FruitSchema().dump(fruit))
-
+    
 
 @app.route("/", methods=["POST"])
 def add_fruit():
-    fruit = Fruit(name=request.json["name"], color=request.json["color"], price = request.json["price"], season = request.json["season"])
+    fruit = Fruit(
+     name = request.json["name"],
+     color = request.json["color"],
+     price = request.json["price"],
+     season = request.json["season"])
     db.session.add(fruit)
     db.session.commit()
-    return jsonify(FruitSchema().dump(fruit))
+    return make_response(jsonify(
+     id = str(fruit.id),
+     name = request.json["name"],
+     color = request.json["color"],
+     price = request.json["price"],
+     season = request.json["season"],
+     status = "success",
+     adress = request.base_url + str(fruit.id)), 
+     201)
 
 
 @app.route("/<id>", methods=["DELETE"])
 def delete_fruit(id):
     fruit = Fruit.query.get(id)
     if not fruit:
-        abort(404)
+        if id in deleted:
+            abort(410)
+        else:
+            abort(404)
+    deleted.add(id)
     db.session.delete(fruit)
     db.session.commit()
-    return jsonify(success=True)
-
+    return  make_response(jsonify(status = "success"), 204)
 
 @app.route("/<id>", methods=["PUT"])
 def update_fruit(id):
@@ -96,7 +128,7 @@ def update_fruit(id):
     fruit.price = request.json["price"]
     fruit.season = request.json["season"]
     db.session.commit()
-    return jsonify(success=True)
+    return make_response(jsonify(status = "success"), 200)
 
 
 if __name__ == '__main__':
